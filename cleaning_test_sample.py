a4a0039c7ee7dac102b6a745c8359628be340eba
Alexandra Magana Noronha <magananoronha@gmail.com>
2017-09-17 09:05:03 -0500

    restructured the database so that url is the key and wrote logic into scraper to prevent dupes

--- cleaning_test_sample.py
+++ cleaning_test_sample.py
@@ -8,24 +8,70 @@ Created on Fri Sep 15 12:53:10 2017
 """
 
 from bs4 import BeautifulSoup
-import requests
 from time import sleep
 import pandas as pd
 import datetime
 import random
 import boto3
-import uuid
-from collections import defaultdict
+
+
+def make_soup(content):
+    soup = BeautifulSoup(content,'lxml')
+    return soup
+
+
+def get_title(soup):
+    title_1 = soup.find_all(class_='entry-title') #watts
+    title_2 = soup.find_all(class_='post-title') #motls
+
+
+    title_tag = soup.find('div', {'class':'post-header'}) 
+        
+
+def get_author(soup):
+    author_1 = soup.find('a', {'class':'author'}) #watts
+    author_2 = soup.find('span', {'class':'post-author'}) #motls
+    author_3 = soup.find('span', {'class':'author-name'}) 
+
+    
+    
+    author_tag = soup.find('a', {'rel':'author'})
+
+    
+def get_pubtime(soup):
+    title_tag = soup.find('meta', {'property':'article:published_time'}) #get the content
+    title_tag = soup.find_all(class_='date-header') #motls
+    author_tag = soup.find('a', {'class':'timestamp-link'}) #motls
+    author_tag = soup.find('abbr', {'class':'published'}) #motls
+
+
+
+def get_body(soup):
+    author_tag = soup.find('div', {'class':'entry-content'}) #watts
+    author_tag = soup.find('div', {'class':'post-body'}) #motls
+
+    
 
 if __name__ == '__main__':
-    sites = defaultdict(list)
-    
+
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('post_metadata')
 
-    for i in range(0,2000):
-        sleep(10)
-        print('Starting Cycle #{}, {} blogs'.format(i, start_urls.count(axis=0)['recent_post']))
+    s3 = boto3.client('s3')
+    
+    response = table.scan()
+    
+    for item in response['Items']:
+        sleep(5)
+        post_uuid = item['uuid']
+        download_time = item['download_time']
+        url = item['url']
+        homepage = item['homepage']
+        
+        content = s3.get_object(Bucket='climateblogs', Key=post_uuid)['Body'].read()
+        soup = make_soup(content)
+        
+        
         for index in range(0,len(start_urls)):
             url = start_urls.loc[index,'recent_post']
             print(index, url)
