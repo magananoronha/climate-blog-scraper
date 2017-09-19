71e6f9fc5fe54d624448f6a7dffbacfe53e0e2ce
Ubuntu <ubuntu@ip-172-31-37-110.us-west-2.compute.internal>
2017-09-19 13:43:02 +0000

    shortening sleep

--- wordpress_scraper.py
+++ wordpress_scraper.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
                   'http://www.drroyspencer.com/']
 
     for i in range(0,2000):
-        sleep_time = random.randint(45,90)
+        sleep_time = random.randint(30,45)
         print('Sleep Time: {} seconds'.format(sleep_time))
         sleep(sleep_time)
         print('Starting Cycle #{}, {} blogs'.format(i, start_urls.count(axis=0)['recent_post']))
