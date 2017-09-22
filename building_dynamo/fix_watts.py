c71b7c203c091c11c2c412ade8add44d1381bd60
Alexandra Magana Noronha <magananoronha@gmail.com>
2017-09-21 20:37:10 -0500

    unstash?

--- building_dynamo/fix_watts.py
+++ building_dynamo/fix_watts.py
@@ -8,6 +8,8 @@ Created on Wed Sep 20 15:00:37 2017
 Some of my early wattsupwiththat posts had a different homepage url.
 
 Successuflly ran the script.  All is well.
+
+Modified the script to fix an issue with the wrong homepage url in realclimate posts.
 """
 
 import boto3
@@ -20,33 +22,34 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('climateblog_metadata')
 
 response = table.scan(
-    FilterExpression=Attr('homepage').eq('wattsupwiththat.com')
+    FilterExpression=Attr('homepage').eq('http://www.climatedepot.com/') & Attr('url').begins_with('https://realclimatescience.com/')
 )
 
 items = response['Items']
 
 while True:
-    print(len(response['Items']))
     sleep(4)
     if response.get('LastEvaluatedKey'):
+        
         response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'],
-                              FilterExpression=Attr('homepage').eq('wattsupwiththat.com'))
-        items += response['Items']
+                              FilterExpression=Attr('homepage').eq('http://www.climatedepot.com/') & Attr('url').begins_with('https://realclimatescience.com/'))
+        print(len(response['Items']))
+        items = response['Items']
+        for item in items:
+            sleep(1)
+            table.update_item(
+                Key={
+                    'url': item['url'],
+                },
+                UpdateExpression='SET homepage = :val1',
+                ExpressionAttributeValues={
+                    ':val1': 'https://realclimatescience.com/'
+                })        
     else:
         break
 
 
-for item in items:
-    sleep(4)
-    table.update_item(
-        Key={
-            'url': item['url'],
-        },
-        UpdateExpression='SET homepage = :val1',
-        ExpressionAttributeValues={
-            ':val1': 'https://wattsupwiththat.com/'
-        }
-    )
+
     
 with open('problem_watts.pkl', 'wb') as fp:
     pickle.dump(items, fp)
