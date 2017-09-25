f246837718f045c16dcc05e78b04676bd449fba3
Alexandra Magana Noronha <magananoronha@gmail.com>
2017-09-25 10:55:01 -0500

    forgot to commit the migration file

--- classes/postgres_migration.py
+++ classes/postgres_migration.py
@@ -13,37 +13,6 @@ import psycopg2
 from customclasses import extractors
 
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('climateblog_metadata')
-
-s3 = boto3.client('s3')
-
-with open('postgres_password.txt') as f:
-    password = f.read().strip()
-
-conn = psycopg2.connect(database='climateblogsdb', user='magananoronha', password=password,
-    host='climateblogdb.cvslli4tn2ay.us-west-2.rds.amazonaws.com', port='5432',
-    connect_timeout=10)
-
-
-password = None
-
-cursor = conn.cursor()
-
-cursor.execute('DROP TABLE blogs CASCADE;')
-cursor.execute('DROP TABLE posts CASCADE;')
-
-with open('../building_postgres/db_schema.sql') as f:
-    sql = f.read()
-    
-cursor.execute(sql)
-
-blog_table = pd.read_csv('classnames.csv')
-
-for i in range(len(blog_table)):
-    cursor.execute( """INSERT INTO blogs (url, cleaning_class) 
-                        VALUES (%s, %s);""",(blog_table.iloc[i]['homepage'],
-                                             blog_table.iloc[i]['className']))
     
 def insert_items(items):
     for i in range(len(items)):
@@ -64,22 +33,58 @@ def insert_items(items):
                                                    result.pub_time,
                                                    result.body))
           
+if __name__ == '__main__':
     
-response = table.scan(Limit=10)
-insert_items(response['Items'])
-
-#while True:
-#    print(len(response['Items']))
-#    sleep(10)
-#    if response.get('LastEvaluatedKey'):
-#        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-#        insert_items(response['Items'])
-#    else:
-#        break
-#
-
-cursor.execute("SELECT * FROM posts;")
-
-tmp = cursor.fetchall()
-
-print(tmp)
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('climateblog_metadata')
+    
+    s3 = boto3.client('s3')
+    
+    with open('postgres_password.txt') as f:
+        password = f.read().strip()
+    
+    conn = psycopg2.connect(database='climateblogsdb', user='magananoronha', password=password,
+        host='climateblogdb.cvslli4tn2ay.us-west-2.rds.amazonaws.com', port='5432',
+        connect_timeout=10)
+    
+    
+    password = None
+    
+    cursor = conn.cursor()
+    
+#    cursor.execute('DROP TABLE blogs CASCADE;')
+#    cursor.execute('DROP TABLE posts CASCADE;')
+#    
+    with open('../building_postgres/db_schema.sql') as f:
+        sql = f.read()
+        
+    cursor.execute(sql)
+    
+    blog_table = pd.read_csv('classnames.csv')
+    
+    for i in range(len(blog_table)):
+        cursor.execute( """INSERT INTO blogs (url, cleaning_class) 
+                            VALUES (%s, %s);""",(blog_table.iloc[i]['homepage'],
+                                                 blog_table.iloc[i]['className']))
+    
+        
+        
+        
+    response = table.scan(Limit=10)
+    insert_items(response['Items'])
+    
+    #while True:
+    #    print(len(response['Items']))
+    #    sleep(10)
+    #    if response.get('LastEvaluatedKey'):
+    #        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+    #        insert_items(response['Items'])
+    #    else:
+    #        break
+    #
+    
+    cursor.execute("SELECT * FROM posts;")
+    
+    tmp = cursor.fetchall()
+    
+    print(tmp)
