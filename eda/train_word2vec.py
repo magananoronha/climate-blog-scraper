4d628eaf98c95f8350f0e24ece5525881f3a36e2
Alexandra Magana Noronha <magananoronha@gmail.com>
2017-10-01 20:47:54 -0500

    cleaning up model training

--- eda/train_word2vec.py
+++ eda/train_word2vec.py
@@ -13,17 +13,16 @@ import os
 if __name__ == '__main__':
     
     data_directory = os.path.join('/home/ubuntu/workspace/climate-blog-scraper/data')
-    trigram_sentences_filepath = os.path.join(data_directory,
-                                              'trigram_sentences_all.txt')
+    final_filepath = os.path.join(data_directory, 'final.txt')
     
-    trigram_sentences = LineSentence(trigram_sentences_filepath)
+    preprocessed_text = LineSentence(final_filepath)
     word2vec_filepath = os.path.join(data_directory, 'word2vec_model_all')
     
-    blog2vec = Word2Vec(trigram_sentences, size=100, window=5,
+    blog2vec = Word2Vec(preprocessed_text, size=100, window=5,
                         min_count=20, sg=1, workers=20)
     
     blog2vec.save(word2vec_filepath)
     
     for i in range(1,12):
-        blog2vec.train(trigram_sentences)
+        blog2vec.train(preprocessed_text)
         blog2vec.save(word2vec_filepath)
